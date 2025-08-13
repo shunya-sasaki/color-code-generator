b8fa93d208849fb684d7b372757809a127e9926a
Shunya Sasaki <shunya.sasaki1120@gmail.com>
2025-08-13 10:07:50 +0900

    feat(models): add default colors, ratios list, and default output path to AppConfig
    Introduce default color palette and ratios list to AppConfig for gradient generation.
    Update docstring to document new fields.
    Set default config file name in to_jsonfile (config.json).

--- src/ccg/models/app_config.py
+++ src/ccg/models/app_config.py
@@ -17,8 +17,9 @@ class AppConfig(BaseModel):
     """Application configuration model.
 
     Args:
-        host (str): Hostname for the application server.
-        port (int): Port number for the application server.
+        colors (list[Color]): List of color object.
+            Each color object contains the name, hex or rgb.
+        ratios (list[float]): List of ratios for generating gradient colors.
         log_level (Literal["debug", "info", "warning", "error"]):
             Logging level for the application.
     """
@@ -26,7 +27,26 @@ class AppConfig(BaseModel):
     model_config = ConfigDict(
         alias_generator=FormatConverter.snake_to_camel, populate_by_name=True
     )
-    colors: list[Color]
+    colors: list[Color] = [
+        Color(name="white", hex="#ffffff"),
+        Color(name="black", hex="#000000"),
+        Color(name="red", hex="#ff0000"),
+        Color(name="green", hex="#00ff00"),
+        Color(name="blue", hex="#0000ff"),
+    ]
+    ratios: list[float] = [
+        -0.90,
+        -0.80,
+        -0.60,
+        -0.40,
+        -0.20,
+        0.0,
+        0.20,
+        0.40,
+        0.60,
+        0.80,
+        0.90,
+    ]
 
     log_level: Literal["debug", "info", "warning", "error"] = "info"
 
@@ -47,7 +67,7 @@ class AppConfig(BaseModel):
             config_json = json.load(file)
         return cls.from_json(config_json)
 
-    def to_jsonfile(self, config_file: str) -> None:
+    def to_jsonfile(self, config_file: str = "config.json") -> None:
         """Save the AppConfig instance to a JSON file."""
         json_str = self.model_dump_json(indent=2, by_alias=True)
         with open(config_file, "w") as fout:
